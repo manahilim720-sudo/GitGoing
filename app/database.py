ce630930466e81e3631368b7e90af580dd137e69
ItsManahil <youremail@example.com>
2026-08-09 07:30:07 -0700

    Add SSL mode for Neon database

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 from urllib.parse import quote_plus
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{quote_plus(settings.database_password)}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{quote_plus(settings.database_password)}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}?sslmode=require'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
